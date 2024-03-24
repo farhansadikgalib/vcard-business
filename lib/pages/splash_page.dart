3079ddf90c3eb4761f7af5ca6684130233803391
farhansadikgalib <farhansadikgalib@gmail.com>
2024-03-24 23:43:58 +0600

    splash page added

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  static const String routeName = 'splash';
+
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircularProgressIndicator(),
+            SizedBox(height: 20),
+            Text('SplashPage'),
+          ],
+        ),
+      ),
+    );
+  }
+}
