4d140de0c6ae483a3d57987f01c2ef9d9306b450
farhansadikgalib <farhansadikgalib@gmail.com>
2024-03-28 23:43:25 +0600

    reformatted

--- lib/providers/contact_provider.dart
+++ lib/providers/contact_provider.dart
@@ -3,46 +3,39 @@ import 'package:vcard/db/db_helper.dart';
 
 import '../models/contact_model.dart';
 
-class ContactsProvider  extends ChangeNotifier{
+class ContactsProvider extends ChangeNotifier {
+  List<ContactModel> contactList = [];
+  final db = DbHelper();
 
-  List <ContactModel> contactList = [];
-  final db =  DbHelper();
-
-  Future<int> insertContact(ContactModel contactModel)async{
-   final rowId =  await db.insertContact(contactModel);
-   contactModel.id = rowId;
-   contactList.add(contactModel);
-   notifyListeners();
-   return rowId;
-
+  Future<int> insertContact(ContactModel contactModel) async {
+    final rowId = await db.insertContact(contactModel);
+    contactModel.id = rowId;
+    contactList.add(contactModel);
+    notifyListeners();
+    return rowId;
   }
-  Future<void> getAllContacts()async{
-    contactList =  await db.getAllContacts();
+
+  Future<void> getAllContacts() async {
+    contactList = await db.getAllContacts();
     notifyListeners();
   }
 
-  Future<void> getAllFavoriteContacts()async{
-    contactList =  await db.getAllFavoriteContacts();
+  Future<void> getAllFavoriteContacts() async {
+    contactList = await db.getAllFavoriteContacts();
     notifyListeners();
   }
 
+  Future<ContactModel> getContactById(int id) => db.getContactById(id);
 
-  Future<ContactModel> getContactById (int id) => db.getContactById(id);
-
-Future<int> deleteContact (int id){
+  Future<int> deleteContact(int id) {
     return db.deleteContact(id);
-}
+  }
 
-Future<void> updateFavorite(ContactModel contactModel)async
-{
-  final value =  contactModel.favorite ? 0 :1;
-  await db.updateFavorite(contactModel.id, value);
-  final index = contactList.indexOf(contactModel);
-  contactList[index].favorite = !contactList[index].favorite;
-  notifyListeners();
-}
-
-
-
-
+  Future<void> updateFavorite(ContactModel contactModel) async {
+    final value = contactModel.favorite ? 0 : 1;
+    await db.updateFavorite(contactModel.id, value);
+    final index = contactList.indexOf(contactModel);
+    contactList[index].favorite = !contactList[index].favorite;
+    notifyListeners();
+  }
 }
